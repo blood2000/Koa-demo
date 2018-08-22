6268bfd54135ccda932768ed5a66b3b06a1bae97
mac <mac@macdeMacBook-Pro.local>
2018-08-22 21:28:05 +0800

    ss

--- db/db.js
+++ db/db.js
@@ -103,7 +103,6 @@ class dbcontroller {
         }
         findobj['_delete'] = { '$ne': true }
         dbunit.changeModelId(findobj)
-        console.log(findobj)
         let count = await collection.find(findobj).count()
         db.close()
         ctx.body = count
@@ -118,87 +117,14 @@ class dbcontroller {
         let collection = db.collection(paramstable)
         let seqid = await db.collection('lb_seq_id').findOneAndUpdate({ id: paramstable }, { $inc: { seq: 1 } }, { upsert: true })
         model.lbseqid = seqid.seq
+        console.log( model.lbseqid)
         dbunit.changeModelId(model)
-        console.log(model)
         let inserted = await collection.insert(model)
         if (!inserted) {
           this.throw(405, 'The model couldn\'t be added.')
         }
         db.close()
         ctx.body = await model
-      }
-
-      async login(ctx){
-        let success = false
-        let paramsdb = ctx.params.db
-        let paramstable = ctx.params.table
-        let querybase64 = ctx.query.q
-        let filterObj = null
-        let data = []
-        let count = 0
-        try {
-          filterObj = JSON.parse(Buffer.from(querybase64, 'base64').toString())
-          success = filterObj.s
-        } catch (error) {
-    
-        }
-        if (success) {
-          let db = await MongoClient.connect(dbunit.getDBStr(paramsdb))
-          let collection = db.collection(paramstable)
-          let options = []
-          let findmatch = false
-          if (filterObj.a) {
-            filterObj.a.forEach((element) => {
-              let obj = {}
-              let type = '$' + element.type
-              obj[type] = element.data || {}
-              dbunit.changeModelId(obj[type])
-              if (type == '$project') {
-                obj[type]._id = 1
-              }
-              if (type == '$match') {
-                findmatch = true
-                obj[type]['_delete'] = { '$ne': true }
-              }
-              options.push(obj)
-            })
-          }
-          if (!findmatch) {
-            let obj = {}
-            obj['$match'] = {}
-            obj['$match']['_delete'] = { '$ne': true }
-            options.push(obj)
-          }
-          options.push({ $group: { _id: null, count: { $sum: 1 } } })
-          let cursor = collection.aggregate(options)
-          let group = await cursor.toArray()
-          if (group && group.length > 0) {
-            count = group[0].count
-          }
-          options.pop()
-    
-          let limit = Number.parseInt(filterObj.prepage || 30)
-          let skip = Number.parseInt(filterObj.page || 0) * limit
-          let sort = filterObj.sort || { '_id': -1 }
-    
-          options.push({ '$sort': sort })
-          options.push({ '$skip': skip })
-          options.push({ '$limit': limit })
-          console.log(options)
-          let tablecursor = collection.aggregate(options)
-          data = await tablecursor.toArray()
-    
-          db.close()
-        }
-        let nowtime = new Date().getTime()
-        console.log(data)
-        ctx.body = await {
-          'data': data,
-          'count': count,
-          'table': paramstable,
-          'nowtime': nowtime
-        }
-
       }
 
       async modify(ctx) {
