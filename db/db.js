e1e251f98294f32962a29ecfb7ec0a33a300a871
mac <mac@macdeMacBook-Pro.local>
2018-08-22 21:32:47 +0800

    ss

--- db/db.js
+++ db/db.js
@@ -116,8 +116,9 @@ class dbcontroller {
         let db = await MongoClient.connect(dbunit.getDBStr(paramsdb))
         let collection = db.collection(paramstable)
         let seqid = await db.collection('lb_seq_id').findOneAndUpdate({ id: paramstable }, { $inc: { seq: 1 } }, { upsert: true })
+        console.log('ssss', seqid)
         model.lbseqid = seqid.seq
-        console.log('ssss', seqid.seq)
+        
         console.log('eeee~~~~',  model.lbseqid )
         dbunit.changeModelId(model)
         let inserted = await collection.insert(model)
